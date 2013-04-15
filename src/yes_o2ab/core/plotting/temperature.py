cdb04a6bf2dc99734093f8d26205da63fb90a145
Craig Versek <cversek@macbuntu.(none)>
2013-04-15 12:41:14 -0400

    plottong for temperature data

---
+++ src/yes_o2ab/core/plotting/temperature.py
@@ -0,0 +1,25 @@
+"""
+spectra.py
+
+"""
+from matplotlib.font_manager import FontProperties
+from automat.core.plotting.plots import MultiPlot
+
+###############################################################################
+class TemperaturePlot(MultiPlot):
+    """ A chart for displaying spectra
+    """
+    def __init__(self,
+                 title      = 'Thermal Monitoring',
+                 xlabel     = r'Time (hours)',
+                 ylabel     = r'Temperature $^{\circ}$C',
+                 styles     = ['r-','g-','b-','c-','y-','m-','k-','r--','g--','b--','c--','y--','m--','k--'],
+                 **kwargs
+                 ):
+        MultiPlot.__init__(self,
+                           title  = title,
+                           xlabel = xlabel,
+                           ylabel = ylabel,
+                           styles = styles,
+                           **kwargs
+                          )

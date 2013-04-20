6231c66fe75247da3e98c8d37bb4b10834af030b
Craig Versek <cversek@o2ab.(none)>
2013-04-19 20:01:37 -0400

    controller for monitoring environmental conditions

--- src/yes_o2ab/drivers/controllers/condition_monitor.py
+++ src/yes_o2ab/drivers/controllers/condition_monitor.py
@@ -18,10 +18,30 @@ class Interface(Controller):
         Controller.__init__(self, **kwargs)
         
     def acquire_sample(self):
-        temps = OrderedDict()
-        for key,therm in sorted(self.devices.items()):
-            temps[therm.name] = therm.read()
-        return temps
+        info = OrderedDict()
+        devices = self.devices.copy() #do not accidently edit in place!
+        camera = devices.pop('camera')
+        sensor_SA_press = devices.pop('sensor_SA_press')
+        sensor_SA_temp  = devices.pop('sensor_SA_temp')
+        sensor_SA_humid = devices.pop('sensor_SA_humid')
+        #read the camera with mutex to avoid inter-thread/process collisions
+        with camera._mutex:
+            info['CC_temp']  = camera.get_CC_temp()
+            info['CH_temp']  = camera.get_CH_temp()
+            info['CC_power'] = camera.get_CC_power()
+        #read DAQ boards with mutex to avoid inter-thread/process collisions
+        with sensor_SA_press.daq._mutex: 
+            info['SA_press_raw_voltage'] = sensor_SA_press.read_raw_voltage()
+        with sensor_SA_temp.daq._mutex: 
+            info['SA_temp_raw_voltage']  = sensor_SA_temp.read_raw_voltage()
+        with sensor_SA_humid.daq._mutex: 
+            info['SA_humid_raw_voltage'] = sensor_SA_humid.read_raw_voltage()
+        #remaining devices should be thermistors
+        for key,therm in sorted(devices.items()):
+            if key.startswith('therm'): #check just in case 
+                with therm.daq._mutex:
+                    info[therm.name] = therm.read()
+        return info
             
     def main(self):
         interval = float(self.configuration['interval'])
@@ -30,18 +50,18 @@ class Interface(Controller):
             info = OrderedDict()
             info['timestamp'] = time.time()
             info['interval']  = interval
-            self._send_event("TEMPERATURE_MONITOR_STARTED",info)
+            self._send_event("CONDITION_MONITOR_STARTED",info)
             #initialize the daq devices
             #daq.initialize()
             while True:
                 t0 = time.time()
                 self._thread_abort_breakout_point()
-                temps = self.acquire_sample()
+                samp = self.acquire_sample()
                 #send information
                 info = OrderedDict()
                 info['timestamp'] = t0
-                info['temperatures'] = temps
-                self._send_event("TEMPERATURE_SAMPLE",info)
+                info.update(samp) #merge in sample dictionary
+                self._send_event("CONDITION_SAMPLE",info)
                 self._thread_abort_breakout_point()
                 # SLEEP UNTIL NEXT CYCLE  ----------------------------
                 used_t = t0 - time.time()
@@ -51,7 +71,7 @@ class Interface(Controller):
                     # END NORMALLY -----------------------------------
                     info = OrderedDict()
                     info['timestamp'] = time.time()
-                    self._send_event("TEMPERATURE_MONITOR_STOPPED",info)
+                    self._send_event("CONDITION_MONITOR_STOPPED",info)
                     return
         except (AbortInterrupt, Exception), exc:
             # END ABNORMALLY -----------------------------------------
@@ -61,7 +81,7 @@ class Interface(Controller):
             info['reason']    = exc
             if not isinstance(exc, AbortInterrupt): 
                 info['traceback'] = traceback.format_exc()
-            self._send_event("TEMPERATURE_MONITOR_ABORTED",info)
+            self._send_event("CONDITION_MONITOR_ABORTED",info)
         finally: #Always clean up!
             pass
             #daq.shutdown()  

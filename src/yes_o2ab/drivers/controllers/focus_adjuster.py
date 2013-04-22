4c72bb6c29b78ec78a311189b21bb95cfd9d8d0a
Craig Versek <cversek@o2ab.(none)>
2013-04-21 21:08:47 -0400

    focus_adjuster controller functionality

---
+++ src/yes_o2ab/drivers/controllers/focus_adjuster.py
@@ -0,0 +1,127 @@
+"""
+Controller to adjust the FLI focuser
+"""
+###############################################################################
+import time, copy
+from automat.core.hwcontrol.controllers.controller import Controller, AbortInterrupt, NullController
+OrderedDict = None
+try:
+    from collections import OrderedDict
+except ImportError:
+    from yes_o2ab.support.odict import OrderedDict
+###############################################################################
+DEFAULT_CONFIGURATION = OrderedDict([
+    ('step_size', 1),
+    ('step_direction', "+1"), #or -1
+])
+
+POLLING_DELAY = 0.1 #seconds
+BABYSTEPSIZE  = 10
+###############################################################################
+class Interface(Controller):
+    def __init__(self,**kwargs):
+        self.position = None
+        Controller.__init__(self, **kwargs)
+        
+    def initialize(self, **kwargs):
+        self.thread_init(**kwargs) #gets the threads working
+        focuser = self.devices['focuser']
+        #send initialize start event
+        info = OrderedDict()
+        info['timestamp'] = time.time()
+        self._send_event("FOCUS_ADJUSTER_INITIALIZE_START", info)
+        try:
+            with focuser._mutex:
+                self.initialize_devices()
+        except RuntimeError, exc: #can't get mutex locks (thread or process level)
+            #send initialize end
+            info = OrderedDict()
+            info['timestamp'] = time.time()
+            info['exception'] = exc
+            self._send_event("FOCUS_ADJUSTER_INITIALIZE_FAIL", info)
+        #success, send initialize end
+        info = OrderedDict()
+        info['timestamp'] = time.time()
+        self._send_event("FOCUS_ADJUSTER_INITIALIZE_END", info)
+        self.query_position()
+    
+    def shutdown(self):
+        pass
+        
+    def query_position(self):
+        focuser = self.devices['focuser']
+        try:
+            with focuser._mutex:
+                self.position = focuser.get_position()
+        except RuntimeError, exc: #can't get mutex locks (thread or process level)
+            info = OrderedDict()
+            info['timestamp'] = time.time()
+            info['exception'] = exc
+            self._send_event("FOCUS_ADJUSTER_QUERY_POSITION_FAIL", info)
+        #success
+        info = OrderedDict()
+        info['timestamp'] = time.time()
+        info['position']  = self.position
+        self._send_event("FOCUS_ADJUSTER_QUERY_POSITION", info)
+        return self.position
+        
+    def main(self):
+        focuser   = self.devices['focuser']
+        step_size = int(self.configuration['step_size'])
+        step_dir  = self.configuration['step_direction'] #"+1" or "-1" as string
+        step_size = int(step_size)
+        step_dir  = int(step_dir)
+        step = step_size*step_dir
+        pos_start = self.query_position()
+        #send initialize start event
+        info = OrderedDict()
+        info['timestamp'] = time.time()
+        info['step']      = step
+        info['start_position']  = pos_start
+        self._send_event("FOCUS_ADJUSTER_STEP_START", info)
+        try:
+            with focuser._mutex:   
+                #focuser.step(step, blocking = False) #FIXME asynchron stepper mode doesn't work!
+                #break a large step into small ones
+                babysteps = step_size // BABYSTEPSIZE * [step_dir*BABYSTEPSIZE] + [step_dir*(step_size % BABYSTEPSIZE)]
+                for babystep in babysteps:
+                    pos = focuser.get_position()
+                    steps_remaining = step - (pos - pos_start)
+                    info = OrderedDict()
+                    info['timestamp'] = time.time()
+                    info['position']  = pos
+                    info['steps_remaining'] = steps_remaining
+                    self._send_event("FOCUS_ADJUSTER_STEP_POLL", info)
+                    #take a babystep
+                    focuser.step(babystep, blocking = True)
+        except RuntimeError, exc: #can't get mutex locks (thread or process level)
+            info = OrderedDict()
+            info['timestamp'] = time.time()
+            info['exception'] = exc
+            self._send_event("FOCUS_ADJUSTER_STEP_FAIL", info)
+        #DEBUG
+        #self.sleep(10) 
+        #success
+        pos_end = self.query_position()
+        info = OrderedDict()
+        info['timestamp'] = time.time()
+        info['step']      = step
+        info['start_position']  = pos_start
+        info['pos_end']   = pos_end
+        self._send_event("FOCUS_ADJUSTER_STEP_END", info)
+        #finish up
+        self.reset() #reset the controller to be used again
+        
+        
+def get_interface(**kwargs):
+    interface_mode = kwargs.pop('interface_mode','threaded')
+    if   interface_mode == 'threaded':
+        return Interface(**kwargs)
+            
+###############################################################################
+# TEST CODE - Run the Controller, collect events, and plot
+###############################################################################
+# FIXME
+if __name__ == "__main__":
+    pass
+

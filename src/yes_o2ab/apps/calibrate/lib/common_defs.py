86254a7682e1fdc9c6ac4144d642a90d566dd54b
Craig Wm. Versek <cversek+github@gmail.com>
2013-04-26 02:06:22 -0400

    factored out shared definitions

---
+++ src/yes_o2ab/apps/calibrate/lib/common_defs.py
@@ -0,0 +1,11 @@
+#Default Parameters
+FRAMETYPE_DEFAULT         = 'normal'
+EXPOSURE_TIME_DEFAULT     = 500
+RBI_NUM_FLUSHES_DEFAULT   = 0
+RBI_EXPOSURE_TIME_DEFAULT = 500
+REPEAT_DELAY_DEFAULT      = 10
+
+#Font Styles
+FIELD_LABEL_FONT      = "Courier 10 normal"
+HEADING_LABEL_FONT    = "Helvetica 14 bold"
+SUBHEADING_LABEL_FONT = "Helvetica 10 bold"

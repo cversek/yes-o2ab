1e271f1b7e5a226baec134964781513026d99cc1
Craig Wm. Versek <cversek+github@gmail.com>
2013-07-06 15:55:00 -0400

    added calibrated corrections

--- src/yes_o2ab/drivers/devices/custom/PTHboard_humidity.py
+++ src/yes_o2ab/drivers/devices/custom/PTHboard_humidity.py
@@ -25,11 +25,12 @@ SENSOR_TO_VOLTAGE = 5.0/(4095)
 ################################################################################
 #class for thermistor object
 class Interface(Device):
-    def __init__(self, daq, daq_channel, name, V0=0):
+    def __init__(self, daq, daq_channel, name, A=0.0, B=1.0):
         self.daq = daq
-        self.daq_channel = daq_channel        
+        self.daq_channel = daq_channel
         self.name = name
-        self.V0   = V0
+        self.A = A
+        self.B = B
         
     def initialize(self):
         pass
@@ -42,10 +43,8 @@ class Interface(Device):
     def read(self):
         "reads the humidity in %RH"
         V = self.read_raw_voltage()
-        V = V + self.V0 #apply voltage correction
-        raise NotImplementedError
-        T = volt_to_temp(V)
-        return T
+        RH = (V/5.0 - self.A)/self.B
+        return RH
         
     def read_raw_voltage(self):
         "reads the uncorrected voltage"
@@ -58,14 +57,17 @@ class Interface(Device):
 #-------------------------------------------------------------------------------
 # INTERFACE CONFIGURATOR         
 def get_interface(**kwargs):
-    daq = kwargs.get('daq')
-    daq_channel = int(kwargs.get('daq_channel'))
-    name = kwargs.get('name')
-    V0   = float(kwargs.get('V0',0.0))
+    daq = kwargs.pop('daq')
+    daq_channel = int(kwargs.pop('daq_channel'))
+    name = kwargs.pop('name')
+    A = float(kwargs.pop('A',0.0))
+    B = float(kwargs.pop('B',1.0))
     iface = Interface(daq=daq,
                       daq_channel=daq_channel,
                       name=name,
-                      V0 = V0
+                      A=A,
+                      B=B,
+                      **kwargs
                      )
     return iface
 ################################################################################
